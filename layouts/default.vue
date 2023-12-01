0fe5043ef1eff77af5112d178ada4b2fb2f43705
Mogos Paul <smash_05@yahoo.com>
2023-12-01 17:36:04 +0200

    First Commit - HomePage

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="px-6 py-3 flex font-sans text-3xl text-white justify-between bg-black text-white h-full border-2 border-amber-900">
+        <div >
+        <NuxtLink to="/" >
+             Home
+        </NuxtLink>
+          
+        <NuxtLink to="/blog" class="px-3 py-3" > 
+            Blog
+        </NuxtLink>
+           
+        <NuxtLink to="/tipstricks" class="px-3 py-3"> 
+            Tips&Tricks
+        </NuxtLink>
+        </div>
+        
+         <div>
+        <NuxtLink to="/about" class="px-3 py-3">
+            About
+        </NuxtLink>
+         </div>
+
+       
+           
+    </div>
+    <slot/>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  
+<div class="flex justify-center bg-black text-white h-screen ">
+   <div class="grid grid-cols-3 grid-rows-3 justify-between  w-4/6 ">
+    <div class="grid col-span-2 row-span-2 h-2/3 text-center ">
+      <div class="text-6xl pt-6 pl-16 h-24">
+          Hey, I'm Paul.
+      </div>
+      <div class="flex flex-col justify-items border-2 ml-5 p-3 shadow-lg shadow-cyan-500/50 border-white">
+          <div class="text-xl  indent-4">
+        I've made this blog to document my journey in learning software engineering. I want to share my progress with everyone and
+        help others on their own paths.
+          </div>
+          <div class="text-xl  pt-4 indent-4">
+        Currently, I mainly know C++, Python and HTML, I am looking forward to learn Rust and hone my JavaScript skills, also competitive programming.
+
+          </div>
+          <div class="text-xl  pt-4 indent-4">
+        There is a Tips&Tricks page where I try to leave useful information about competitive programming and also general computer science.
+         I mainly participate in contests on <a href="https://codeforces.com" class="underline">Codeforces</a>.
+        
+          </div>
+      </div>
+      
+      
+      
+    </div>
+  <div class="grid  justify-items-center">
+    
+    <div class="w-48 pt-12">
+      <img src="photos\cpplogo.png">
+    </div>
+    <div class="w-48">
+      <img src="photos\pylogo.png">
+    </div>
+    <div class="w-48">
+      <img src="photos\html.png">
+    </div>
+  </div>
+  </div>
+</div>
+ 
+
+</template>
